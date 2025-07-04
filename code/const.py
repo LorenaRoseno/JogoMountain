6a6f2eea83e5ebd1e0d6f90949e23c96037e9f3f
Lorena Roseno <rosenolorena.fin@gmail.com>
2025-07-04 11:42:53 -0300

    Creating Player and Enemy shot

--- code/const.py
+++ code/const.py
@@ -1,11 +1,42 @@
-#C
+# C
 import pygame
 
-COLOR_ORANGE = (255,128,0)
-COLOR_WHITE = (255,255,255)
-COLOR_YELLOW =(255,255,0)
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (255, 255, 0)
 
-#E
+# E
+ENTITY_HEALTH = {'Level1Bg0': 999,
+                 'Level1Bg1': 999,
+                 'Level1Bg2': 999,
+                 'Level1Bg3': 999,
+                 'Level1Bg4': 999,
+                 'Level1Bg5': 999,
+                 'Level1Bg6': 999,
+                 'Level2Bg0': 999,
+                 'Level2Bg1': 999,
+                 'Level2Bg2': 999,
+                 'Level2Bg3': 999,
+                 'Level2Bg4': 999,
+                 'Level2Bg5': 999,
+                 'Level2Bg6': 999,
+                 'Player1': 300,
+                 'Player1Shot': 1,
+                 'Player2': 300,
+                 'Player2Shot': 1,
+                 'Enemy1': 50,
+                 'Enemy1Shot': 3,
+                 'Enemy2': 60,
+                 'Enemy2Shot': 3,
+}
+
+ENTITY_SHOT_DELAY = {
+    'Player1': 20,
+    'Player2': 15,
+    'Enemy1': 100,
+    'Enemy2': 200,
+}
+
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
@@ -14,35 +45,19 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
-    'Player1' : 3,
-    'Player2' : 3,
-    'Enemy1' : 2,
-    'Enemy2' : 1,
+    'Player1': 3,
+    'Player1Shot': 3,
+    'Player2': 3,
+    'Player2Shot': 3,
+    'Enemy1': 1,
+    'Enemy1Shot': 5,
+    'Enemy2': 1,
+    'Enemy2Shot': 2,
 }
 
-ENTITY_HEALTH = {'Level1Bg0' : 999,
-    'Level1Bg1' : 999,
-    'Level1Bg2' : 999,
-    'Level1Bg3' : 999,
-    'Level1Bg4' : 999,
-    'Level1Bg5' : 999,
-    'Level1Bg6' : 999,
-    'Level2Bg0' : 999,
-    'Level2Bg1' : 999,
-    'Level2Bg2' : 999,
-    'Level2Bg3' : 999,
-    'Level2Bg4' : 999,
-    'Level2Bg5' : 999,
-    'Level2Bg6' : 999,
-    'Player1' : 300,
-    'Player2' : 300,
-    'Enemy1' : 50,
-    'Enemy2' : 60
-}
+EVENT_ENEMY = pygame.USEREVENT + 1
 
-EVENT_ENEMY = pygame.USEREVENT +1
-
-#M
+# M
 MENU_OPTION = ('--NEW GAME-- (1 PLAYER)',
                '-NEW GAME- (2 PLAYERS - COOPERATIVE)',
                '-NEW GAME- (2 PLAYERS - COMPETITIVE)',
@@ -53,19 +68,18 @@ MENU_OPTION = ('--NEW GAME-- (1 PLAYER)',
 PLAYER_KEY_UP = {'Player1': pygame.K_UP,
                  'Player2': pygame.K_w}
 PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
-                 'Player2': pygame.K_s}
+                   'Player2': pygame.K_s}
 PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
-                 'Player2': pygame.K_a}
+                   'Player2': pygame.K_a}
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
-                 'Player2': pygame.K_d}
+                    'Player2': pygame.K_d}
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
-                 'Player2': pygame.K_LCTRL}
+                    'Player2': pygame.K_LCTRL}
 
-#S
+# S
 
 SPAWN_TIME = 4000
 
-#W
+# W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
-
--- code/level.py
+++ code/level.py
@@ -6,9 +6,11 @@ from pygame import Surface, Rect
 from pygame.font import Font
 
 from code.const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
+from code.enemy import Enemy
 from code.entity import Entity
 from code.entityFactory import EntityFactory
 from code.entityMediator import EntityMediator
+from code.player import Player
 
 
 class Level:
@@ -33,6 +35,11 @@ class Level:
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+                if isinstance(ent, (Player, Enemy)):
+                    shoot = ent.shoot()
+                    if shoot is not None:
+                        self.entity_list.append(shoot)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -58,12 +65,3 @@ class Level:
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
         self.window.blit(source=text_surf, dest=text_rect)
-
-
-
-
-
-
-
-
-
